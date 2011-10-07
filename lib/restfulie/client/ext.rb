286bc2956bef6b6b5d38b65c1e4d477f1bc16aa2
Hemant Kumar <gethemant@gmail.com>
2011-10-07 07:59:11 +0530

    Do not patch Net::HTTP for 1.9.3

--- lib/restfulie/client/ext.rb
+++ lib/restfulie/client/ext.rb
@@ -1,3 +1,6 @@
 require 'restfulie/client/ext/link_ext'
-require 'restfulie/client/ext/http_ext'
 require 'restfulie/client/ext/open_search_ext'
+
+if RUBY_VERSION < "1.9.3"
+  require 'restfulie/client/ext/http_patch_ext'
+end

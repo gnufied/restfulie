8b98c58584e9812f93d2c12844366d77ba553c4b
Hemant Kumar <gethemant@gmail.com>
2011-10-10 09:22:45 +0530

    Do not load for 1.9.2 as well

--- lib/restfulie/client/ext.rb
+++ lib/restfulie/client/ext.rb
@@ -1,6 +1,6 @@
 require 'restfulie/client/ext/link_ext'
 require 'restfulie/client/ext/open_search_ext'
 
-if RUBY_VERSION < "1.9.3"
+if RUBY_VERSION < "1.9.2"
   require 'restfulie/client/ext/http_patch_ext'
 end
